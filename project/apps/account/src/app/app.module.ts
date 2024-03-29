1553f9ad1ae63c7e355745e62a48503dbb05c787
Nikolay Blinov <arcmag-work@mail.ru>
2024-03-30 02:17:00 +0700

    creates a new library - account/authentication

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from '@project/authentication';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule],
   controllers: [],
   providers: [],
 })
