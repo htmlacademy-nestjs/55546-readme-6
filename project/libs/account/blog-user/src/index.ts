7479d468f8613e127a5a80317167b303089e38cb
Nikolay Blinov <arcmag-work@mail.ru>
2024-03-30 18:10:56 +0700

    updates the export and connection of data from the library blog-user

--- project/libs/account/blog-user/src/index.ts
+++ project/libs/account/blog-user/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/blog-user';
+export { BlogUserModule } from './blog-user-module/blog-user.module';
+export { BlogUserRepository } from './blog-user-module/blog-user.repository';
