155957ae7e8c790fe173bcce79b898e2a97010b9
Nikolay Blinov <arcmag-work@mail.ru>
2024-03-30 20:50:15 +0700

    Adds a basic swagger including

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 const PORT = 3000;
@@ -15,6 +15,15 @@ const GLOBAL_PREFIX = 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const config = new DocumentBuilder()
+    .setTitle('Account Application')
+    .setDescription('The Readme site the account application description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || PORT;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
