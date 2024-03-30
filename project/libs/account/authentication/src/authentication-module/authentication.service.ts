5ba677d96554dc7b5f70ad523e5fe99555d626a7
Nikolay Blinov <arcmag-work@mail.ru>
2024-03-30 18:48:16 +0700

    adds a method for user login to the authentication service

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,7 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { BlogUserRepository } from '@project/blog-user';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './authentication.constants';
+import { BlogUserEntity } from '@project/blog-user';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) { }
+
+  public async register(dto: CreateUserDto) {
+    const { email, password, avatar, name } = dto;
+
+    const blogUser = {
+      email,
+      name,
+      avatar,
+      passwordHash: '',
+      registrationDate: null
+    }
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+    if (existUser) {
+      throw new Error(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    this.blogUserRepository.save(userEntity);
+
+    return userEntity;
+  }
 }
