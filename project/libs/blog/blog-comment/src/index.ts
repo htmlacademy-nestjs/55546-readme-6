3264f2b983ddfcde9e1372fa2415be17a0989ad2
Nikolay Blinov <arcmag-work@mail.ru>
2024-04-19 15:38:56 +0700

    updates the blog post entity

--- project/libs/blog/blog-comment/src/index.ts
+++ project/libs/blog/blog-comment/src/index.ts
@@ -1,1 +1,2 @@
 export { BlogCommentModule } from './blog-comment-module/blog-comment.module';
+export { BlogCommentEntity } from './blog-comment-module/blog-comment.entity';
--- project/libs/blog/blog-post/src/blog-post-module/blog-post.entity.ts
+++ project/libs/blog/blog-post/src/blog-post-module/blog-post.entity.ts
@@ -1,5 +1,7 @@
-import { CommonPostType, Entity, LinkPost, PhotoPost, Post, PostStatus, PostType, QuotePost, TextPost, VideoPost } from '@project/shared/core';
+import { CommonPostType, Entity, LinkPost, PhotoPost, PostStatus, PostType, QuotePost, TextPost, VideoPost } from '@project/shared/core';
 import { StorableEntity } from '@project/shared/core';
+import { BlogCommentEntity } from '@project/blog-comment';
+import { BlogCommentFactory } from 'libs/blog/blog-comment/src/blog-comment-module/blog-comment.factory';
 
 function isPhotoType(obj: any): obj is PhotoPost {
   return obj.type === PostType.Photo;
@@ -26,11 +28,19 @@ export class BlogPostEntity extends Entity implements StorableEntity<CommonPostT
   public status: PostStatus;
   public authorId: string;
   public title: string;
-  public tags: string[];
-  public likes: string[];
-  public dateCreate: Date;
-  public dateUpdate: Date;
+  public tags?: string[];
+  public likes?: string[];
+  public dateCreate?: Date;
+  public dateUpdate?: Date;
   public isReposted: boolean;
+  public comments: BlogCommentEntity[];
+
+  public photo?: string;
+  public text?: string;
+  public announcement?: string;
+  public quoteAuthor?: string;
+  public link?: string;
+  public description?: string;
 
   constructor(post?: CommonPostType) {
     super();
@@ -43,16 +53,39 @@ export class BlogPostEntity extends Entity implements StorableEntity<CommonPostT
     }
 
     this.id = post.id ?? undefined;
+    this.title = post.title ?? undefined;
     this.type = post.type ?? undefined;
     this.status = post.status ?? undefined;
     this.authorId = post.authorId ?? undefined;
     this.tags = post.tags ?? [];
     this.likes = post.likes ?? [];
-    this.dateCreate = post.dateCreate ?? undefined;
-    this.dateUpdate = post.dateUpdate ?? undefined;
+    this.dateCreate = post.dateCreate ?? new Date();
+    this.dateUpdate = post.dateUpdate ?? new Date();
 
-    if ('title' in post) {
-      this.title = post.title;
+    const blogCommentFactory = new BlogCommentFactory();
+    this.comments = post.comments.map(comment => blogCommentFactory.create(comment));
+
+    if (isPhotoType(post)) {
+      this.photo = post.photo ?? undefined;
+    }
+
+    if (isTextType(post)) {
+      this.text = post.text ?? undefined;
+      this.announcement = post.announcement ?? undefined;
+    }
+
+    if (isQuoteType(post)) {
+      this.text = post.text ?? undefined;
+      this.quoteAuthor = post.quoteAuthor ?? undefined;
+    }
+
+    if (isLinkType(post)) {
+      this.link = post.link ?? undefined;
+      this.description = post.description ?? undefined;
+    }
+
+    if (isVideoType(post)) {
+      this.link = post.link ?? undefined;
     }
   }
 
@@ -67,7 +100,8 @@ export class BlogPostEntity extends Entity implements StorableEntity<CommonPostT
       likes: this.likes,
       dateCreate: this.dateCreate,
       dateUpdate: this.dateUpdate,
-      isReposted: this.isReposted
+      isReposted: this.isReposted,
+      comments: this.comments.map(comment => comment.toPOJO())
     };
 
     if (isPhotoType(this)) {
@@ -87,7 +121,9 @@ export class BlogPostEntity extends Entity implements StorableEntity<CommonPostT
     if (isLinkType(this)) {
       basePost.link = this.link;
       basePost.description = this.description;
-    } else if (isVideoType(this)) {
+    }
+
+    if (isVideoType(this)) {
       basePost.link = this.link;
     }
 
